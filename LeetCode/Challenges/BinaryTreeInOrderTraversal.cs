b1c71c24bd11dc043bd132d79291be2e7a5bcd2a
Gabriel Fernandez <gabriel.fernandez@logisticsflow.com>
2024-06-27 10:15:25 -0400

    BTree Inorder traversal

---
+++ LeetCode/Challenges/BinaryTreeInOrderTraversal.cs
@@ -0,0 +1,52 @@
+﻿namespace LeetCode.Challenges
+{
+    /// <summary>
+    /// Given the root of a binary tree, return the inorder traversal of its nodes' values.
+    /// </summary>
+    public class BinaryTreeInOrderTraversal : ILeetCode
+    {
+        private List<int> _nodes = [];
+
+        public IList<int> Run(TreeNode root)
+        {
+            if (root == null)
+            {
+                return _nodes;
+            }
+
+            if (root.left == null && root.right == null)
+            {
+                _nodes.Add(root.val);
+                return _nodes;
+            }
+
+            Traverse(root);
+
+            return _nodes;
+        }
+
+        private void Traverse(TreeNode? node)
+        {
+            if (node == null)
+            {
+                return;
+            }
+
+            Traverse(node.left);
+
+            _nodes.Add(node.val);
+
+            Traverse(node.right);
+        }
+    }
+
+    /// <summary>
+    /// Definition for a binary tree node.
+    /// </summary>
+    public class TreeNode(int val = 0, TreeNode? left = null, TreeNode? right = null)
+    {
+        public int val = val;
+        public TreeNode? left = left;
+        public TreeNode? right = right;
+    }
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,3 +1,9 @@
 ﻿using LeetCode.Challenges;
 
+var t3 = new TreeNode(3, null, null);
+var t2 = new TreeNode(2, null, t3);
+var t1 = new TreeNode(1, null, t2);
+
+new BinaryTreeInOrderTraversal().Run(t1);
+
 Console.ReadLine();

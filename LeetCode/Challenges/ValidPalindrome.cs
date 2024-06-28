cb688181913bd6c98f94a3d34460f85471cd29e0
Gabriel Fernandez <gabriel.fernandez@logisticsflow.com>
2024-06-28 09:22:58 -0400

    ValidPalindrome

---
+++ LeetCode/Challenges/ValidPalindrome.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+
+namespace LeetCode.Challenges
+{
+    /// <summary>
+    /// A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward.
+    /// Alphanumeric characters include letters and numbers.
+    /// Given a string s, return true if it is a palindrome, or false otherwise.
+    /// </summary>
+    public class ValidPalindrome : ILeetCode
+    {
+        public bool Run(string s)
+        {
+            var alphaNumeric = new StringBuilder();
+
+            foreach (char c in s)
+            {
+                if (('A' <= c && c <= 'Z') || ('a' <= c && c <= 'z') || ('0' <= c && c <= '9'))
+                {
+                    alphaNumeric.Append(c);
+                }
+            }
+
+            var array = alphaNumeric.ToString().ToLower().ToCharArray();
+
+            for (int i = 0; i < array.Length / 2; i++)
+            {
+                if (array[i] != array[array.Length - 1 - i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,9 +1,5 @@
 ﻿using LeetCode.Challenges;
 
-var t3 = new TreeNode(3, null, null);
-var t2 = new TreeNode(2, null, t3);
-var t1 = new TreeNode(1, null, t2);
-
-new BinaryTreeInOrderTraversal().Run(t1);
+new ValidPalindrome().Run("0P");
 
 Console.ReadLine();

95c3beb32c37449de68cbd39ce13584ffb782d4f
Gabriel Fernandez <gabriel.fernandez@logisticsflow.com>
2024-07-02 10:19:47 -0400

    LongestPalindromicSubString

---
+++ LeetCode/Challenges/Medium/LongestPalindromeSubstring.cs
@@ -0,0 +1,55 @@
+﻿using System.Text;
+
+namespace LeetCode.Challenges.Medium
+{
+    public class LongestPalindromeSubstring : ILeetCode
+    {
+        public string Run(string s)
+        {
+            var results = new StringBuilder();
+
+            if (!string.IsNullOrEmpty(s))
+            {
+                results.Append(s[0].ToString());
+                var resultsLength = 1;
+
+                for (int i = 0; i < s.Length; i++)
+                {
+                    var leftPointer = i;
+                    var rightPointer = i;
+
+                    while (leftPointer >= 0 && rightPointer < s.Length && s[leftPointer] == s[rightPointer])
+                    {
+                        if (rightPointer - leftPointer + 1 > resultsLength)
+                        {
+                            results.Clear();
+                            results.Append(s.Substring(leftPointer, rightPointer - leftPointer + 1));
+                            resultsLength = rightPointer - leftPointer + 1;
+                        }
+
+                        leftPointer--;
+                        rightPointer++;
+                    }
+
+                    leftPointer = i;
+                    rightPointer = i + 1;
+
+                    while (leftPointer >= 0 && rightPointer < s.Length && s[leftPointer] == s[rightPointer])
+                    {
+                        if (rightPointer - leftPointer + 1 > resultsLength)
+                        {
+                            results.Clear();
+                            results.Append(s.Substring(leftPointer, rightPointer - leftPointer + 1));
+                            resultsLength = rightPointer - leftPointer + 1;
+                        }
+
+                        leftPointer--;
+                        rightPointer++;
+                    }
+                }
+            }
+
+            return results.ToString();
+        }
+    }
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,10 +1,5 @@
-﻿using LeetCode.Challenges.Easy;
-using LeetCode.Challenges.Shared;
+﻿using LeetCode.Challenges.Medium;
 
-var tree = new TreeNode(5, new TreeNode(3, new TreeNode(2), new TreeNode(4)), new TreeNode(8, null, new TreeNode(9)));
-
-var preOrder = new BinaryTreePreOrderTraversal().Run(tree);
-var inOrder = new BinaryTreeInOrderTraversal().Run(tree);
-var postOrder = new BinaryTreePostOrderTraversal().Run(tree);
+new LongestPalindromeSubstring().Run("caba");
 
 Console.ReadLine();
